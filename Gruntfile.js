1291b6e789cbd21e18499f00bcde65f7e2e9184e
Remco Tolsma <info@remcotolsma.nl>
2014-05-12 09:56:49 +0200

    Added Grunt copy for to create an deploy directory.

--- Gruntfile.js
+++ Gruntfile.js
@@ -25,14 +25,34 @@ module.exports = function(grunt) {
 
     // Before generating any new files, remove any previously-created files.
     clean: {
+      deploy: {
+        src: ['deploy']
+      },
       tests: ['tmp']
+    },
+
+    // Copy
+	copy: {
+      deploy: {
+        src: [
+          '**',
+          '!.*',
+          '!.*/**',
+          '!Gruntfile.js',
+          '!package.json',
+          '!node_modules/**',
+		],
+        dest: 'deploy',
+        expand: true,
+        dot: true
+      },
     },
 
     // Configuration to be run (and then tested).
     rt_wp_deploy: {
-      woocommerce_nl: {
+      app: {
         options: {
-          svnUrl: 'http://plugins.svn.wordpress.org/woocommerce-nl/',
+          svnUrl: 'https://grunt-rt-wp-deploy.googlecode.com/svn/',
           svnDir: 'svn',
           deployDir: 'deploy'
         }
@@ -51,6 +71,7 @@ module.exports = function(grunt) {
 
   // These plugins provide necessary tasks.
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-nodeunit');
 
@@ -60,6 +81,7 @@ module.exports = function(grunt) {
 
   // By default, lint and run all tests.
   //grunt.registerTask('default', ['jshint', 'test']);
+  grunt.registerTask('deploy', ['clean:deploy','copy:deploy']);
   grunt.registerTask('default', ['rt_wp_deploy']);
 
 };
